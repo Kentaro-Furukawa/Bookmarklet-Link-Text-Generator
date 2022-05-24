7174e1dcaf048df9786f126583a4786ccde85d17
Ken <info@ken89.com>
2022-05-25 06:15:14 +0900

    shorten the code of attern 6

--- src/pattern-6.js
+++ src/pattern-6.js
@@ -2,19 +2,18 @@ javascript: (() => {
     const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;
     const pageTitle = document.title;
     const incNo = pageTitle.match(incPattern);
-    if (!(incNo)) {
-        console.log('Invalid page title.')
-    } else {
+    if (incNo) {
         const link = `[${incNo[0]}](https://example.com/${incNo[0]})`;
         (async () => {
             try {
                 await navigator.clipboard.writeText(link)
             } catch (error) {
-                console.log('Error: heee')
+                console.log(`Error 😗 : ${error}`)
             }
         })()
     }
 })()
 
 
-javascript: (() => {const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;const pageTitle = document.title;const incNo = pageTitle.match(incPattern);if (!(incNo)) {console.log('Invalid page title.')} else {const link = `[${incNo[0]}](https://example.com/${incNo[0]})`;(async () => {try {await navigator.clipboard.writeText(link)} catch (error) {console.log('Error 😗')}})()}})()
+javascript: (() => {const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;const pageTitle = document.title;const incNo = pageTitle.match(incPattern);if (incNo) {const link = `[${incNo[0]}](https://example.com/${incNo[0]})`;(async () => {try {await navigator.clipboard.writeText(link)} catch (error) {console.log(`Error 😗 : ${error}`)}})()}})()
+
