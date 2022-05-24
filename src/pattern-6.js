720a39a146d00f6dafbe828cfdb411cfb5c12693
Ken <info@ken89.com>
2022-05-25 06:10:10 +0900

    add pattern 6 😗
    add async and try catch to pattern 1.

---
+++ src/pattern-6.js
@@ -0,0 +1,20 @@
+javascript: (() => {
+    const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;
+    const pageTitle = document.title;
+    const incNo = pageTitle.match(incPattern);
+    if (!(incNo)) {
+        console.log('Invalid page title.')
+    } else {
+        const link = `[${incNo[0]}](https://example.com/${incNo[0]})`;
+        (async () => {
+            try {
+                await navigator.clipboard.writeText(link)
+            } catch (error) {
+                console.log('Error: heee')
+            }
+        })()
+    }
+})()
+
+
+javascript: (() => {const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;const pageTitle = document.title;const incNo = pageTitle.match(incPattern);if (!(incNo)) {console.log('Invalid page title.')} else {const link = `[${incNo[0]}](https://example.com/${incNo[0]})`;(async () => {try {await navigator.clipboard.writeText(link)} catch (error) {console.log('Error 😗')}})()}})()
